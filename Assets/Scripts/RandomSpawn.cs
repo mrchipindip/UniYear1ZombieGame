f7155093d461a295f3aeac02edee0a97a9714e91
Chris Wesemael <chrislbw@hotmail.co.uk>
2014-10-23 23:27:04 +0100

    added new zombie type, need to add the random spawner position

---
+++ Assets/Scripts/RandomSpawn.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomSpawn : MonoBehaviour {
+	public GameObject Spawner;
+
+	public float MinX = -23.8f;
+	public float MaxX = 23.8f;
+	public float MinY = -23.8f;
+	public float MaxY = 23.8f;
+	// Use this for initialization
+	void Start () {
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//float x = Random.Range (MinX, MaxX);
+		//float y = Random.Range (MinY, MaxY);
+
+		//Instantiate(Spawner, new Vector3 (x,y,0), Quaternion.identity);
+	}
+}
