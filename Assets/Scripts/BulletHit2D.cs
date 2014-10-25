1c5e1058f0881c79d13379e956fb52b6a6d9d903
Chris Wesemael <chrislbw@hotmail.co.uk>
2014-10-25 15:40:38 +0100

    BUilt Menu screen, adde zombie type 2's scripts and colliders, dies now . need to add a spawner for it. added more spawners for regs. dont have the scene switching working atm

--- Assets/Scripts/BulletHit2D.cs
+++ Assets/Scripts/BulletHit2D.cs
@@ -5,11 +5,15 @@ public class BulletHit2D : MonoBehaviour {
 
 	public int damage = 1;
 	public string damageTag = "";
+	public string damageTag2 = "";
 
 	void OnTriggerEnter2D(Collider2D other){
 				if (other.CompareTag (damageTag)) {
 						other.SendMessage ("TakeDamage", damage);
 				}
+		else if (other.CompareTag (damageTag2)) {
+			other.SendMessage ("TakeDamage", damage);
+		}
 				Destroy (gameObject);
 		}
 
---
+++ Assets/Scripts/PlayGameButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayGameButton : MonoBehaviour {
+
+	public string levelName = "Zombie Shooter Level 1";
+
+	void OnMouseUp () {
+		Application.LoadLevel (levelName);
+		}
+}
