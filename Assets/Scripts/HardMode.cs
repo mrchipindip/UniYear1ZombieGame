3d9ca5ba7763e75a4fc03f2f9bf10042ecb1c356
Chris Wesemael <chrislbw@hotmail.co.uk>
2014-11-03 15:17:09 +0000

    Added easy medium and hard mode, need to fix death sound

---
+++ Assets/Scripts/HardMode.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HardMode : MonoBehaviour {
+	
+	public string levelName = "ZombieShooterHArd";
+	
+	void OnMouseUp () {
+		Application.LoadLevel (levelName);
+	}
+}
---
+++ Assets/Scripts/MainMenuButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuButton : MonoBehaviour {
+	
+	public string levelName = "Main Menu Scene";
+	
+	void OnMouseUp () {
+		Application.LoadLevel (levelName);
+	}
+}
