8b085783eeda2a9610ad8652f04f3bd3381f7af1
Chris Wesemael <chrislbw@hotmail.co.uk>
2014-12-16 00:38:05 +0000

    changed the fusrohdah so that it shoots on the middle mouse button, so that it is more compatable with the web. also adde a second build with this

--- Assets/Scripts/ShootFusRoDah.cs
+++ Assets/Scripts/ShootFusRoDah.cs
@@ -31,7 +31,7 @@ public class ShootFusRoDah : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if(Input.GetMouseButtonDown (1)){
+		if(Input.GetMouseButtonDown (2)){
 			audio.Play ();
 			if(!isFiring){
 				//if(audio){
