06c5c89c126147f149d54d48b9a23f6839986a08
Chris Wesemael <chrislbw@hotmail.co.uk>
2015-01-27 01:47:27 +0000

    Fixed win condition so that it is now working

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Victory : MonoBehaviour {
+	public string levelName = "Main Menu Scene";
+
+	public void Enabler(int damage) {
+		Application.LoadLevel (levelName);
+		}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
